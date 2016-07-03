5cd9d09df7e6b2ee9a0ae44f6d607ccc0eef8d13
Dominik Toton <dtoton@wp.pl>
2016-07-03 13:47:03 +0200

    reorganized DataCollectorImpl class

--- src/com/github/shemhazai/mprw/data/DataCollectorImpl.java
+++ src/com/github/shemhazai/mprw/data/DataCollectorImpl.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Calendar;
 import java.util.Date;
 
 import org.jsoup.Connection;
@@ -31,8 +30,79 @@ public class DataCollectorImpl implements DataCollector {
 	@Autowired
 	private RiverStatusRepository riverStatusRepository;
 
+	private static final String DATE_FORMAT = "HH:mm-dd.MM.yyyy";
+
+	private static final int DATE_COLUMN_INDEX = 0;
+	private static final int LEVEL_COLUMN_INDEX = 1;
+
 	public DataCollectorImpl() {
 
+	}
+
+	public void collect() throws IOException, ParseException {
+		for (River river : riverRepository.selectAllRivers())
+			collectFromRiver(river);
+	}
+
+	@Transactional
+	private void collectFromRiver(River river)
+			throws IOException, ParseException {
+		String urlString = createUrlString(river.getName());
+		Connection connection = Jsoup.connect(urlString);
+		Document document = connection.get();
+		for (Element row : selectTableRows(document))
+			parseTableRow(river, row);
+	}
+
+	private String createUrlString(String riverName) {
+		return (baseUrl.endsWith("/") ? baseUrl : baseUrl + "/")
+				+ "wizualizacja/punkt_pomiarowy.php?prze=" + riverName;
+	}
+
+	private Elements selectTableRows(Document document) {
+		return document.select("tr[title]");
+	}
+
+	private void parseTableRow(River river, Element row) throws ParseException {
+		Elements columns = selectTableColumns(row);
+		if (columns.size() != 3)
+			throw new ParseException("The table does not have three columns", 0);
+
+		DateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT);
+		Date date = dateFormat.parse(dateString(columns));
+
+		if (!existsRiverStatus(river.getId(), date))
+			insertStatus(river.getId(), date, parseLevel(columns));
+	}
+
+	private Elements selectTableColumns(Element row) {
+		return row.select("td");
+	}
+
+	private String dateString(Elements columns) {
+		DateFormat formatter = new SimpleDateFormat("yyyy");
+		String year = formatter.format(new Date());
+		return columns.get(DATE_COLUMN_INDEX).text() + "." + year;
+	}
+
+	private boolean existsRiverStatus(int riverId, Date date) {
+		return riverStatusRepository.existsRiverStatusWithRiverIdAndDate(
+				riverId, date);
+	}
+
+	private int parseLevel(Elements columns) {
+		try {
+			return Integer.parseInt(columns.get(LEVEL_COLUMN_INDEX).text());
+		} catch (NumberFormatException e) {
+			return 0;
+		}
+	}
+
+	private void insertStatus(int riverId, Date date, int level) {
+		RiverStatus status = riverStatusRepository.createRiverStatus(riverId);
+		status.setDate(date);
+		status.setLevel(level);
+		riverStatusRepository.updateRiverStatus(status.getId(), status);
 	}
 
 	public String getBaseUrl() {
@@ -57,54 +127,5 @@ public class DataCollectorImpl implements DataCollector {
 
 	public void setRiverStatusRepository(RiverStatusRepository riverStatusRepository) {
 		this.riverStatusRepository = riverStatusRepository;
-	}
-
-	public void collect() throws IOException, ParseException {
-		for (River river : riverRepository.selectAllRivers())
-			collectFromRiver(river);
-	}
-
-	@Transactional
-	private void collectFromRiver(River river)
-			throws IOException, ParseException {
-		String year = Calendar.getInstance().get(Calendar.YEAR) + "";
-		String urlString = createUrlString(river.getName());
-
-		Connection connection = Jsoup.connect(urlString);
-		Document document = connection.get();
-
-		Elements rows = document.select("tr[title]");
-		for (Element row : rows) {
-			Elements columns = row.select("td");
-			if (columns.size() != 3)
-				throw new ParseException("The table does not have three columns", 0);
-
-			DateFormat dateFormat = new SimpleDateFormat("HH:mm-dd.MM.yyyy");
-			Date date = dateFormat.parse(columns.get(0).text() + "." + year);
-
-			if (riverStatusRepository.existsRiverStatusWithRiverIdAndDate(
-					river.getId(), date))
-				continue;
-
-			RiverStatus riverStatus = riverStatusRepository.createRiverStatus(
-					river.getId());
-			riverStatus.setDate(date);
-
-			int level;
-			try {
-				level = Integer.parseInt(columns.get(1).text());
-			} catch (NumberFormatException e) {
-				level = 0;
-			}
-			riverStatus.setLevel(level);
-
-			riverStatusRepository.updateRiverStatus(riverStatus.getId(),
-					riverStatus);
-		}
-	}
-
-	private String createUrlString(String riverName) {
-		return (baseUrl.endsWith("/") ? baseUrl : baseUrl + "/")
-				+ "wizualizacja/punkt_pomiarowy.php?prze=" + riverName;
 	}
 }
